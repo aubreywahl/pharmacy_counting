5c8af21c0e3b237adc4bec042d747581c8fd184b
Aubrey Wahl <aawahl@gmail.com>
2018-07-17 21:27:16 -0700

    fix bug (dumb typo) in first path thru input

--- src/pharmacy_counting/__init__.py
+++ src/pharmacy_counting/__init__.py
@@ -3,6 +3,19 @@ import csv
 from itertools import groupby
 from args import parse_commandline_args
 
+# gives one level of array-flattening
+flatten = lambda l: [item for sublist in l for item in sublist]
+
+def count_and_sum(cost_dict):
+  total = 0
+  count = 0
+  for k in cost_dict:
+    total += cost_dict[k]
+    count += 1
+
+  return count, total
+  
+
 def main():
   # get input/output file paths
   path_in, path_out = parse_commandline_args()
@@ -13,29 +26,40 @@ def main():
 
     reader = csv.DictReader(f)
 
+    # read relevant info from dataset
     for row in reader:
       
       drug  = row["drug_name"]
       fname = row["prescriber_first_name"]
       lname = row["prescriber_last_name"]
-      cost  = row["drug_cost"]
+      cost  = float(row["drug_cost"])
       
-      if (drug not in row):
+      if (drug not in info_grouped_by_drug):
         info_grouped_by_drug[drug] = {}
+
 
       cost_by_prescriber = info_grouped_by_drug[drug]
 
       tup = (fname, lname)
 
+      # print("CPB before:" ,cost_by_prescriber)
+
       # if cost has not been recorded, record it
       if (tup not in cost_by_prescriber):
         cost_by_prescriber[tup] = cost
+        # print("CBP after:", cost_by_prescriber)
       
       # greedily use highest-cost value
       elif (cost_by_prescriber[tup] < cost):
         cost_by_prescriber[tup] = cost
 
-  print(info_grouped_by_drug.keys())
+      info_grouped_by_drug[drug] = cost_by_prescriber
+
+
+  print(info_grouped_by_drug)
+  unsorted_output = [flatten([[k], count_and_sum(info_grouped_by_drug[k])]) for k in info_grouped_by_drug]
+
+  print(unsorted_output)
 
   # TODO: make this a real thing!
   output_text = """drug_name,num_prescriber,total_cost
--- src/pharmacy_counting/__main__.py
+++ src/pharmacy_counting/__main__.py
@@ -2,8 +2,8 @@ from __init__ import main
 from memory_profiler import memory_usage
 
 if __name__ == '__main__':
-  
-  mem_usage = memory_usage(main)
-  print('Memory usage (in chunks of .1 seconds): %s' % mem_usage)
-  print('Maximum memory usage: %s' % max(mem_usage))  
+  main()
+  # mem_usage = memory_usage(main)
+  # print('Memory usage (in chunks of .1 seconds): %s' % mem_usage)
+  # print('Maximum memory usage: %s' % max(mem_usage))  
 
